42b6d263c918d673e7362782f647146e7cc9a096
MementoXMori777 <sebastianus777@gmail.com>
2021-03-11 20:50:29 +0100

    update

--- wp-content/themes/trener-personalny/loop.php
+++ wp-content/themes/trener-personalny/loop.php
@@ -1,47 +1,43 @@
-<div class="news-blog">
-  <div class="container">
-		<div class="news-blog__loop">
-			<div class="row">
-			<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
-				<!-- article -->
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<!-- article -->
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-					<!-- post thumbnail -->
-					<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-							<?php the_post_thumbnail(array(350,220)); ?>
-						</a>
-					<?php endif; ?>
-					<!-- /post thumbnail -->
+		<!-- post thumbnail -->
+		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
+			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+				<?php the_post_thumbnail(array(120,120)); // Declare pixel size you need inside the array ?>
+			</a>
+		<?php endif; ?>
+		<!-- /post thumbnail -->
 
-					<!-- post title -->
-					<div class="loop-content-data">
-						<p class="date"><?php the_time('F j, Y'); ?></p>
-						<a class="main-heading" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+		<!-- post title -->
+		<h2>
+			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+		</h2>
+		<!-- /post title -->
 
-						<p><?php the_excerpt(); ?></p>
+		<!-- post details -->
+		<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
+		<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
+		<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
+		<!-- /post details -->
 
-				        <a class="read-more-link-blog" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">Czytaj więcej <i class="fas fa-angle-double-right"></i></a>
-					</div>
-					<!-- /post title -->
+		<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
 
-					<?php edit_post_link(); ?>
+		<?php edit_post_link(); ?>
 
-				</article>
-				<!-- /article -->
-			<?php endwhile; ?>
+	</article>
+	<!-- /article -->
 
-			<?php else: ?>
+<?php endwhile; ?>
 
-				<!-- article -->
-				<article>
-					<h2><?php _e( 'Nie ma dodanych żadnych wpisów', 'html5blank' ); ?></h2>
-				</article>
-				<!-- /article -->
+<?php else: ?>
 
-			<?php endif; ?>
-			</div>
-		</div>
-	</div>
-</div>
+	<!-- article -->
+	<article>
+		<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+	</article>
+	<!-- /article -->
+
+<?php endif; ?>
